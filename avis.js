068d8cc811ac37c4ab9d10bc700c43507612335d
Samath24 <tdelmas24@gmail.com>
2024-06-04 13:10:15 +0200

    Préparation, suite

--- avis.js
+++ avis.js
@@ -1,9 +1,11 @@
 export function ajoutListenersAvis() {
-  const piecesElements = document.querySelectorAll(".fiches article button");
+  const piecesElements = document.querySelectorAll(".fiches button");
 
   for (let i = 0; i < piecesElements.length; i++) {
     piecesElements[i].addEventListener("click", async function (event) {
-         /* ... */
+      const id = event.target.dataset.id;
+      console.log(id);
+      /* fetch(`http://localhost:8081/pieces/${id}/avis`); */
     });
   }
 }
